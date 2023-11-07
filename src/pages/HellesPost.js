ad97aa2e6ba72f01b3378b8391dbeca6490d238b
anninapohl19 <annina.pohl@gmail.com>
2023-11-07 13:57:37 +0100

    Changed the name, pic and follow container

--- src/pages/HellesPost.js
+++ src/pages/HellesPost.js
@@ -3,17 +3,15 @@ import './App.css';
 import TopBarPost from './components/TopBarPost.js';
 import Footer from './components/Footer';
 import PostDetail from './components/PostDetail.js';
-import ProfilePictureDetailedPost from './components/ProfilePictureDetailedPost.js';
 import FollowNameContainer from './components/FollowNameContainer';
 import DetailedPostDescription from './components/DetailedPostDescription';
 import Tags from './components/Tags';
 
-function App() {
+function HellesPost() {
   return (
     <div className="App">
       <PostDetail/>
       <TopBarPost/>
-      <ProfilePictureDetailedPost/>
       <FollowNameContainer/>
       <DetailedPostDescription/>
       <Tags tags={["Aurlandsdalen", "Norway", "Hiking", "Wild", "Spectacular Nature", "Outdoor"]}/>
