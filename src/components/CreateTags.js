e5f7d4a94412638e0260ef84bb11ba360cd2d4d4
anninapohl19 <annina.pohl@gmail.com>
2023-11-15 14:33:19 +0100

    CreatePost Changes

--- src/components/CreateTags.js
+++ src/components/CreateTags.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreateTags.css';
 
 function CreateTags(props) {
+  const [inputValue, setInputValue] = useState('');
 
-    return (
-        <ul className="create-tags">
-            {props.tags.map((tag, index) => (
-            <li className="tag" key={index}>
-                {tag}
-            </li>
-            ))}
-        </ul>
-    );
-  }
-  
-  export default CreateTags;
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInputKeyPress = (e) => {
+    if (e.key === 'Enter' && inputValue.trim() !== '') {
+      // Add the new tag to the parent component's state
+      props.onSpecialTagChange([...props.tags, inputValue.trim()]);
+      // Clear the input field
+      setInputValue('');
+    }
+  };
+
+  return (
+    <ul className="create-tags">
+      {props.predefinedTags.map((tag, index) => (
+        <li className="tag" key={index}>
+          {tag}
+        </li>
+      ))}
+      {props.tags.map((tag, index) => (
+        <li className="tag user-defined" key={index}>
+          {tag}
+          <span className="delete-tag" onClick={() => props.onSpecialTagChange(props.tags.filter((_, i) => i !== index))}>
+            &times;
+          </span>
+        </li>
+      ))}
+      <li className="tag special-tag">
+        {/* Input field for the special tag */}
+        <input
+          type="text"
+          placeholder="Create tag"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyPress={handleInputKeyPress}
+        />
+      </li>
+    </ul>
+  );
+}
+
+export default CreateTags;
