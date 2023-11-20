a193baeed51dd07cad353bc61c159e960462c707
anninapohl19 <annina.pohl@gmail.com>
2023-11-20 16:17:47 +0100

    Added Routing before and after creating a post

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import './Footer.css'; 
-import Home from "../Ressources/Icons/HomeIcon.svg";
-import Create from "../Ressources/Icons/CreatePost.svg";
-import Search from "../Ressources/Icons/SearchIcon.svg";
-import ProfilePic from "../Ressources/ProfilePictures/OwnProfilePicture.png";
+import './Footer.css';
+import Home from '../Ressources/Icons/HomeIcon.svg';
+import Create from '../Ressources/Icons/CreatePost.svg';
+import Search from '../Ressources/Icons/SearchIcon.svg';
+import ProfilePic from '../Ressources/ProfilePictures/OwnProfilePicture.png';
+import { useNavigate } from 'react-router-dom';
 
 function Footer() {
-    return (
-      <footer className="footer">
-        <div className="footer-icon"><img src={Home} alt="HomeButton" /></div>
-        <div className="footer-icon"><img src={Create} alt="CreatePost" /></div>
-        <div className="footer-icon"><img src={Search} alt="SearchIcon" /></div>
-        <div className="profile-icon"><img src={ProfilePic} alt="UserProfile" /></div>
-      </footer>
-    );
-  }
-  
-  export default Footer;
+  const navigate = useNavigate(); // Add this line to use the navigate function
+
+  const handleCreatePostClick = () => {
+    navigate('/create-post'); // Navigate to the create post page
+  };
+
+  return (
+    <footer className="footer">
+      <div className="footer-icon">
+        <img src={Home} alt="HomeButton" />
+      </div>
+      <div className="footer-icon" onClick={handleCreatePostClick}>
+        <img src={Create} alt="CreatePost" />
+      </div>
+      <div className="footer-icon">
+        <img src={Search} alt="SearchIcon" />
+      </div>
+      <div className="profile-icon">
+        <img src={ProfilePic} alt="UserProfile" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
+
 
   
